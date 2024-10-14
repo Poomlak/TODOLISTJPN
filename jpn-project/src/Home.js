d84a70cdd2a2d9651e02c5cf149fc00215ae34d5
Nanfha 6430300480 <29418nanfha@gmail.com>
2024-10-14 15:39:01 +0700

    Home V3

--- jpn-project/src/Home.js
+++ jpn-project/src/Home.js
@@ -1,10 +1,20 @@
 import React from "react";
-import Navbar from "./Navbar";
+import jpnLogo from './jpn_logo.png';
+import './Home.css';
 
 const Home = () => {
   return (
-    <div className="home">
-      <h1>JPN</h1>
+    <div className="center-content">
+      <img src={jpnLogo} alt="JPN Todolist Logo" className="logo" />
+      <h2 className="tagline">ควบคุมการจัดตารางเวลาง่ายๆโดยใช้</h2>
+      <h2 className="app-title">JPN Todolist</h2>
+      <p className="description">
+        Become focused, organized, and calm with todo app. The World's #1 task manager app.
+      </p>
+      <div className="buttons">
+        <button className="sign-in">Sign In</button>
+        <button className="sign-up">Sign Up</button>
+      </div>
     </div>
   );
 };
