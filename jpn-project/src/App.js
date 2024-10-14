ced8eefb2fecef389edae46d8c573865b1b9f8a2
Poomlak <mrpoomkakmak@hotmail.com>
2024-10-14 16:16:53 +0700

    Update path Home

--- jpn-project/src/App.js
+++ jpn-project/src/App.js
@@ -1,8 +1,19 @@
 import logo from "./logo.svg";
+import Home from "./Home";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
-  return <h1>JPN Todolist</h1>;
+  return (
+    <BrowserRouter>
+      <h1>JPN Todolist</h1>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
