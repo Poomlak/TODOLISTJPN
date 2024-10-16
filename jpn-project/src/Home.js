68d69cc7e07965bbaf47b2e5904389b022566484
Poomlak <mrpoomkakmak@hotmail.com>
2024-10-16 14:54:44 +0700

    navigate ปุ่ม signup,in

--- jpn-project/src/Home.js
+++ jpn-project/src/Home.js
@@ -1,19 +1,32 @@
 import React from "react";
-import jpnLogo from './navbars/jpn_logo.png';
-import './Home.css';
-
+import jpnLogo from "./navbars/jpn_logo.png";
+import "./Home.css";
+import { useNavigate } from "react-router-dom";
 const Home = () => {
+  const navigate = useNavigate(); // useNavigate ต้องทำงานภายใน <Router>
+
+  const goSignIn = () => {
+    navigate("/signin");
+  };
+  const goSignup = () => {
+    navigate("/signup");
+  };
   return (
     <div className="center-content">
       <img src={jpnLogo} alt="JPN Todolist Logo" className="logo" />
       <h2 className="tagline">ควบคุมการจัดตารางเวลาง่ายๆโดยใช้</h2>
       <h2 className="app-title">JPN Todolist</h2>
       <p className="description">
-        Become focused, organized, and calm with todo app. The World's #1 task manager app.
+        Become focused, organized, and calm with todo app. The World's #1 task
+        manager app.
       </p>
       <div className="buttons">
-        <button className="sign-in">Sign In</button>
-        <button className="sign-up">Sign Up</button>
+        <button className="sign-in" onClick={goSignIn}>
+          Sign In
+        </button>
+        <button className="sign-up" onClick={goSignup}>
+          Sign Up
+        </button>
       </div>
     </div>
   );
