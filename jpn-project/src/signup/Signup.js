ee2171b63275e30695180f301e258c0e85d65deb
Poomlak <mrpoomkakmak@hotmail.com>
2024-10-16 14:34:09 +0700

    แก้ UI Signup

--- jpn-project/src/signup/Signup.js
+++ jpn-project/src/signup/Signup.js
@@ -9,14 +9,55 @@ const Signup = () => {
         <img src={jpnLogo} alt="JPN Todolist Logo" className="logo" />
         <h1 className="app-title">Create an account</h1>
       </div>
+
       <div className="form-container">
         <form className="signup-form">
-          <input type="text" placeholder="Username" />
-          <input type="password" placeholder="Password" />
-          <input type="password" placeholder="Re-password" />
-          <input type="date" placeholder="Birthday" />
-          <input type="email" placeholder="E-mail" />
-          <input type="tel" placeholder="Telephone" />
+          <div className="input-group">
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              placeholder="Enter your username"
+            />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter your password"
+            />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="repassword">Re-password:</label>
+            <input
+              type="password"
+              id="repassword"
+              placeholder="Re-enter your password"
+            />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="birthday">Birthday:</label>
+            <input type="date" id="birthday" />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="email">E-mail:</label>
+            <input type="email" id="email" placeholder="Enter your email" />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="telephone">Telephone:</label>
+            <input
+              type="tel"
+              id="telephone"
+              placeholder="Enter your phone number"
+            />
+          </div>
+
           <button className="sign-up">Sign Up</button>
         </form>
       </div>
