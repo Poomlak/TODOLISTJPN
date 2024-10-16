2b01bb3bab10b3d1d277ced1eb5c8e3386c5ced0
Nanfha 6430300480 <29418nanfha@gmail.com>
2024-10-16 15:29:31 +0700

    แก้ signin เพิ่ม show password

--- jpn-project/src/signin/Signin.js
+++ jpn-project/src/signin/Signin.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import jpnLogo from "../navbars/jpn_logo.png";
 import "./Signin.css";
-import axios from "axios"; 
+import axios from "axios";
 
 const Signin = () => {
   const [formData, setFormData] = useState({
@@ -23,18 +23,17 @@ const Signin = () => {
     e.preventDefault();
 
     try {
-        const response = await axios.post('http://localhost:5000/login', {
-            username: formData.username,
-            password: formData.password
-        });
-        alert('Login successful');
-        
+      const response = await axios.post('http://localhost:5000/login', {
+        username: formData.username,
+        password: formData.password
+      });
+      console.log(response)
+      alert('Login successful');
     } catch (error) {
-        console.error('Error logging in:', error.response.data);
-        alert('Invalid username or password');
+      console.error('Error logging in:', error.response.data);
+      alert('Invalid username or password');
     }
-};
-
+  };
 
   return (
     <div className="signin-container">
@@ -73,7 +72,15 @@ const Signin = () => {
                 className="toggle-password-button"
                 onClick={togglePasswordVisibility}
               >
-                
+                <img
+                  src={
+                    showPassword
+                      ? "https://cdn.discordapp.com/attachments/861532175925116928/1296001688376709130/image.png?ex=6710b321&is=670f61a1&hm=535724526a15ce2bbfea95b3f1785a3044f4336224c942bbe641957bede828c1&"
+                      : "https://cdn.discordapp.com/attachments/861532175925116928/1296001359476166726/image.png?ex=6710b2d2&is=670f6152&hm=18d411368535db5cc89775501579453130649b5b4fbfc6d94298b5496aef480d&"
+                  }
+                  alt="Toggle Password Visibility"
+                  className="password-toggle-icon"
+                />
               </button>
             </div>
           </div>
@@ -81,7 +88,7 @@ const Signin = () => {
             <button type="submit" className="signin-button">
               Sign in
             </button>
-            <button type="submit" className="sign_up">
+            <button type="button" className="sign_up">
               Sign Up
             </button>
           </div>
